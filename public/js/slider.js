57279eff7f99b4c82021b328b31430f734fcd5e0
Danila <Danila00000@Gmail.com>
2017-11-24 17:01:04 +0700

    slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
-	$('.next').click(function(){
-		var currentImage=$('.img.curry');
-		var currentImageIndex=$('.img.curry').index();
-		var nextImageIndex=currentImageIndex + 1;
-		var nextImage= $('.img').eq(nextImageIndex);
-		currentImage.fadeOut(1000);
-		currentImage.removeClass('curry');
 
-		if(nextImageIndex == ($('.img:last').index()+1)){
-			$('.img').eq(0).fadeIn(1000);
-			$('.img').eq(0).addClass('curry');
+	$('[data-slider-nav]').click(function(){
+		var $this=$(this),
+			$slider=$this.closest('[data-slider]'),
+			$sliderWrap=$slider.find('[data-slider-wrap]'),
+			direction=$this.attr('data-slider-nav'),
+			$sliderPrev=$("[data-slider-nav=prev]"),
+			$sliderNext=$("[data-slider-nav=next]"),
+			current=$slider.attr("data-slider"); //определяем текущий слайдер
+
+
+		if (!current) current=0;
+		current=parseInt(current);
+		if (direction=="next") {
+			current+=1;
 		} else {
-			nextImage.fadeIn(1000);
-			nextImage.addClass('curry')
+			current-=1;
 		}
-	});
+		$slider.attr("data-slider", current);
+		$sliderWrap.css("left",current*-100+"%");
 
-	$('.prev').click(function(){
-		var currentImage=$('.img.curry');
-		var currentImageIndex=$('.img.curry').index();
-		var prevImageIndex=currentImageIndex - 1;
-		var prevImage= $('.img').eq(prevImageIndex);
+		$sliderPrev.removeAttr("disabled");
+		$sliderNext.removeAttr("disabled");
 
-		currentImage.fadeOut(1000);
-		currentImage.removeClass('curry');
-		prevImage.fadeIn(1000);
-		prevImage.addClass('curry');
+		// конец левого слайда
+		if (current==0) $sliderPrev.attr("disabled","disabled");
+
+		// конец правого слайда
+		if (current==($sliderWrap.find(".slide").length-1)) $sliderNext.attr("disabled","disabled");
 	});
 	
 });
